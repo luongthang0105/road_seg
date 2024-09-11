9aec1ce7f5d96cac15355749890a57dc4364f7ce
alialphamu <ali.wahab@alphamudigital.com>
2024-09-11 14:19:34 +0500

    removed os operation from test cases

--- model/solutions_test.py
+++ model/solutions_test.py
@@ -2,7 +2,6 @@ from model.solution import Solution
 from model.line_segment import LineSegment
 from model.point import Point
 import pytest
-import os
 
 
 @pytest.fixture
@@ -43,30 +42,3 @@ def test_add_solution_with_two_solution(solution) -> None:
         Point(-2.1, 5.3, 0.16),
     ])])
     assert len(solution.solutions) == 2
-
-
-def test_to_json(solution) -> None:
-    try:
-        solution.add_solution("basic.ply", [LineSegment([
-            Point(-2.9, -0.67, 0.16),
-            Point(0.85, -0.74, 0.16),
-            Point(5.2, -2.5, 0.16),
-            Point(7.6, 0.13, 0.16),
-            Point(5.7, 3.6, 0.16),
-            Point(2.2, 4.9, 0.16),
-            Point(-2.1, 5.3, 0.16),
-        ])])
-        solution.add_solution("bendy.ply", [LineSegment([
-            Point(-2.9, -0.67, 0.16),
-            Point(0.85, -0.74, 0.16),
-            Point(5.2, -2.5, 0.16),
-            Point(7.6, 0.13, 0.16),
-            Point(5.7, 3.6, 0.16),
-            Point(2.2, 4.9, 0.16),
-            Point(-2.1, 5.3, 0.16),
-        ])])
-
-        solution.to_json()
-        assert os.path.exists("./data/tests/solutions.json") == True
-    finally:
-        os.remove("./data/tests/solutions.json")
