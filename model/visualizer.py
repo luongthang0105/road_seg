a2650aa6ad1213e9c276a171db78071d31e22a62
Sahan Fernando <sahan@neara.com>
2024-10-01 14:55:10 +1000

    Handle close button in visualizer

--- model/visualizer.py
+++ model/visualizer.py
@@ -37,11 +37,13 @@ class NonBlockingVisualizer:
             self.line_set.colors = updated_line_set.colors
 
             self.vis.update_geometry(self.line_set)
-            self.vis.poll_events()
+            if not self.vis.poll_events():
+                break
             self.vis.update_renderer()
 
             time.sleep(self.update_interval)
             t += self.update_interval
+        self.stop()
 
     def stop(self):
         self.running = False
